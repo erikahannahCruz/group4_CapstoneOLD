00e5c51e4d10f27b3924666c80358fa1710872d6
p1p1pupu <11erikacruz11@gmail.com>
2022-04-07 20:33:18 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/group4_capstone/Map_MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.group4_capstone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem ;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Map_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        //Bottom Button Navigation
+
+        //Initialize and assign variable
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        //Set Map selected
+        bottomNavigationView.setSelectedItemId(R.id.Map);
+
+        //Perform item selected listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.Schedule:
+                        startActivity (new Intent (getApplicationContext(), Schedule.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.Map:
+                        startActivity (new Intent (getApplicationContext(), Map_MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.Profile:
+                        startActivity (new Intent (getApplicationContext(), Profile.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.Settings:
+                        startActivity (new Intent (getApplicationContext(), Settings.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false ;
+            }
+
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/group4_capstone/Settings.java
@@ -0,0 +1,58 @@
+package com.example.group4_capstone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        //Bottom Button Navigation
+
+        //Initialize and assign variable
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        //Set Settings selected
+        bottomNavigationView.setSelectedItemId(R.id.Settings);
+
+        //Perform item selected listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.Schedule:
+                        startActivity (new Intent(getApplicationContext(), Schedule.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.Map:
+                        startActivity (new Intent (getApplicationContext(), Map_MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.Profile:
+                        startActivity (new Intent (getApplicationContext(), Profile.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.Settings:
+                        startActivity (new Intent (getApplicationContext(), Settings.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false ;
+            }
+
+        });
+
+    }
+}
